fd5db1ea9a9ecb8c29960aae8cd116f9551bf336
Mauricio Blum <mauriciocblum@gmail.com>
2022-01-21 12:24:46 +0100

    (build): Added vercel info and other things

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -25,6 +25,7 @@ export const loader = () => {
 };
 
 import type { MetaFunction } from 'remix';
+import GithubIcon from '../components/GithubIcon';
 
 export const meta: MetaFunction = ({ data }: { data: Data | undefined }) => {
   if (!data) {
@@ -46,14 +47,19 @@ export default function Index() {
   const goodMorningOfTheDay = data.list[data.selectedIndex];
 
   return (
-    <div className="flex flex-col items-center justify-center text-center h-screen bg-gray-100, px-5">
+    <section className="flex flex-col items-center justify-center text-center h-screen px-5">
+      <div className="flex absolute right-3 top-3">
+        <a className="flex items-center" href="https://github.com/mauricioblum/good-morning">
+          <span className="mr-2">View on GitHub</span> <GithubIcon />
+        </a>
+      </div>
       <p className="text-center text-gray-700 md:text-8xl text-6xl font-bold transition-all">
         {goodMorningOfTheDay.flag}
       </p>
-      <h1 className="md:text-8xl text-6xl font-bold mb-8 transition-all">
+      <h1 className="md:text-8xl text-6xl font-bold mb-8 transition-all max-w-full">
         {goodMorningOfTheDay.phrase}
       </h1>
-      <p className="md:text-2xl text-xl px-4 transition-all">{goodMorningOfTheDay.description}</p>
-    </div>
+      <p className="md:text-xl text-lg px-4 transition-all">{goodMorningOfTheDay.description}</p>
+    </section>
   );
 }
